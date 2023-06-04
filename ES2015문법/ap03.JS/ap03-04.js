17caeaf7ba86b199a0a75b66a7899f73812adc29
sejoong <sjydevil@gmail.com>
2023-06-04 14:20:02 +0900

    2023-06-04 14:20

---
+++ ES2015문법/ap03.JS/ap03-04.js
@@ -0,0 +1,23 @@
+function name(params) {
+  console.log('name function');
+}
+const name2 = function (params) {
+  console.log('name2 function');
+};
+const obj = {
+  a: 1,
+  name2: name2,
+};
+
+obj.name2();
+
+
+const b = new Number(10);
+console.log(b);
+console.log(b.toString());
+
+debugger;
+
+const a = 10; // ==> Number Object
+console.log(a);
+console.log(a.toString());
